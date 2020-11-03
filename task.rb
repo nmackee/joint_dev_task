83ec2c78e252796c1a36d6836853e69926917d46
nmackee <mackee.base@gmail.com>
2020-11-04 00:15:23 +0900

    Ruby課題(基礎)Q6,Q8,Q10の回答を修正

--- task.rb
+++ task.rb
@@ -60,9 +60,7 @@ def q6
   numbers1 = [1, 2, 3, 4, 5]
 
   # 以下に回答を記載
-  numbers2 = numbers1.map {
-    |i| i*10
-  }
+  numbers2 = numbers1.map { |i| i*10 } 
   p numbers2
 
 end
@@ -81,6 +79,7 @@ def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
+  programming_languages.map!(&:capitalize)
   upper_case_programming_languages = programming_languages.map(&:upcase)
   # 以下は変更しないで下さい
   p programming_languages
@@ -101,9 +100,8 @@ def q10
   foods = %w(いか たこ うに しゃけ うにぎり うに軍艦 うに丼)
 
   # 以下に回答を記載
-  str = "うに"
   foods.each do |food|
-    if food.include?(str)
+    if food.include?("うに")
       puts "好物です"
     else
       puts "まあまあ好きです"
